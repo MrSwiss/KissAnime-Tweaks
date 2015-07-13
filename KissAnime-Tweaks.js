94258deeced2f61f3f72fa142a596fe7ae223b37
Lukas <KiraLT@users.noreply.github.com>
2015-07-13 17:51:55 +0300

    Update KissAnime-Tweaks.js

--- KissAnime-Tweaks.js
+++ KissAnime-Tweaks.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KissAnime Tweaks
 // @namespace    https://github.com/KiraLT
-// @version      0.1
+// @version      0.2
 // @description  KissAnime page Tweaks
 // @author       KiraLT
 // @match        http://kissanime.com/*
@@ -19,5 +19,11 @@ jQuery('#divAds').remove();
 jQuery('#divAds2').remove();
 
 // Premium in navigation
-jQuery('#menu_box a[href="/Message/GetKPoint"]').html('&nbsp;&nbsp;<img src="http://kissanime.com/Content/images/kpoint.png">&nbsp;KPoint: 1000000');
+jQuery('#menu_box a[href="/Message/GetKPoint"]').html('&nbsp;&nbsp;<img src="http://kissanime.com/Content/images/kpoint.png">&nbsp;KPoint: 1000000').attr('href', '#');
 jQuery('#menu_box a[href="/Message/GoPremium"]').html('&nbsp;&nbsp;<img src="http://kissanime.com/Content/images/genre.png">&nbsp;Premium for ever').attr('href', '#');
+
+// Index page treaks
+if (window.location.pathname === '/') {
+    // Remove "Remove ads" panel
+    jQuery('.barTitle:contains("Remove ads")').parent().remove();
+}
