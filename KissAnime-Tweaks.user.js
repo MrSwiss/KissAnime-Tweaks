a3764d98a229626f4ceb0ad7345cd38faeb5da28
Lukas <KiraLT@users.noreply.github.com>
2015-07-20 14:46:41 +0300

    Update KissAnime-Tweaks.user.js

--- KissAnime-Tweaks.user.js
+++ KissAnime-Tweaks.user.js
@@ -1,29 +1,48 @@
 // ==UserScript==
 // @name         KissAnime Tweaks
 // @namespace    https://github.com/KiraLT
-// @version      0.2
+// @version      0.3
 // @description  KissAnime page Tweaks
 // @author       KiraLT
 // @match        http://kissanime.com/*
 // @grant        None
 // ==/UserScript==
 
-// Disable Adblock Detection
 DoDetect2 = function(){};
 
-// Hide ads
-jQuery('.divCloseBut a').click();
-jQuery('#divFloatLeft').remove();
-jQuery('#divFloatRight').remove();
-jQuery('#divAds').remove();
-jQuery('#divAds2').remove();
+function tweaks_remove_ads() {
+    jQuery('.divCloseBut a').click();
+    jQuery('#divFloatLeft').remove();
+    jQuery('#divFloatRight').remove();
+    jQuery('#divAds').remove();
+    jQuery('#divAds2').remove();
+}
 
-// Premium in navigation
-jQuery('#menu_box a[href="/Message/GetKPoint"]').html('&nbsp;&nbsp;<img src="http://kissanime.com/Content/images/kpoint.png">&nbsp;KPoint: 1000000').attr('href', '#');
-jQuery('#menu_box a[href="/Message/GoPremium"]').html('&nbsp;&nbsp;<img src="http://kissanime.com/Content/images/genre.png">&nbsp;Premium for ever').attr('href', '#');
+function tweaks_remove_top_social() {
+    jQuery('#formSearch div:nth-child(1)').remove();
+}
 
-// Index page treaks
+function tweaks_clean_index() {
+    jQuery('.bigBarContainer').next().remove();
+    jQuery('.bigBarContainer').next().remove();
+    jQuery('.bigBarContainer').next().remove();
+}
+
+function tweaks_remove_panels() {
+    var names = ["Remove ads", "Like me"];
+    for (var i = 0; i < names.length; i++) {
+        jQuery('.barTitle:contains("' + names[i] + '")').parent().remove();
+    }
+}
+
+function tweaks_clean_anime() {
+    jQuery('.episodeList div:first').next().find('div').not('table').remove();
+}
+
+tweaks_remove_ads();
+tweaks_remove_top_social();
+tweaks_clean_anime();
 if (window.location.pathname === '/') {
-    // Remove "Remove ads" panel
-    jQuery('.barTitle:contains("Remove ads")').parent().remove();
+    tweaks_remove_panels();
+    tweaks_clean_index();
 }
